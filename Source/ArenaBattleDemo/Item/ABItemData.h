c7c684f73609bead96614f53d9688d0ebad1f360
fel <fel.forest3910@gmail.com>
2025-04-23 11:43:17 +0900

    [feat] 보상 상자 보상을 랜덤하게 설정
    - AssetManager을 이용해 특정 타입 에셋을 가져오가 만들어 리스트에 저장 후 랜덤하게 호출

--- Source/ArenaBattleDemo/Item/ABItemData.h
+++ Source/ArenaBattleDemo/Item/ABItemData.h
@@ -21,6 +21,15 @@ UCLASS()
 class ARENABATTLEDEMO_API UABItemData : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
+
+public:
+    // FPrimaryAssetId 값을 반환하는 GetPrimaryAssetId 함수 오버라이드
+    // 첫번째 인자 : 태그값.
+    // 두번째 인자 : 이름값, 일반적으로 GetFname()을 많이 활용함
+    virtual FPrimaryAssetId GetPrimaryAssetId() const override
+    {
+        return FPrimaryAssetId("ABItemData", GetFName());
+    }
 	
 public:
     // 아이템 타입을 가지는 데이터
--- Source/ArenaBattleDemo/Item/ABWeaponItemData.h
+++ Source/ArenaBattleDemo/Item/ABWeaponItemData.h
@@ -15,6 +15,15 @@ class ARENABATTLEDEMO_API UABWeaponItemData : public UABItemData
 	GENERATED_BODY()
 
 public:
+	// FPrimaryAssetId 값을 반환하는 GetPrimaryAssetId 함수 오버라이드
+	// 첫번째 인자 : 태그값.
+	// 두번째 인자 : 이름값, 일반적으로 GetFname()을 많이 활용함
+	virtual FPrimaryAssetId GetPrimaryAssetId() const override
+	{
+		return FPrimaryAssetId("ABItemData", GetFName());
+	}
+
+public:
 	// 제공할 무기에 대한 스켈레탈 메시.
 	UPROPERTY(EditAnywhere, Category = Weapon)
 	TSoftObjectPtr<USkeletalMesh> WeaponMesh;
