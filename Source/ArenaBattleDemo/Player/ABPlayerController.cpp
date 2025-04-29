8f6533c839c151f3a7a7fba2153dd56def27cf0c
fel <fel.forest3910@gmail.com>
2025-04-29 11:17:23 +0900

    [feat] UIWidget 생성 및 배치

--- Source/ArenaBattleDemo/Player/ABPlayerController.cpp
+++ Source/ArenaBattleDemo/Player/ABPlayerController.cpp
@@ -1,7 +1,19 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Player/ABPlayerController.h"
+#include "UI/ABHUDWidget.h"
+
+AABPlayerController::AABPlayerController()
+{
+	// 위젯 블루프린트 에셋에서 클래스 정보 로드
+	static ConstructorHelpers::FClassFinder<UABHUDWidget> ABHUDWidgetRef(TEXT("/Game/ArenaBattle/UI/WBP_ABHUD.WBP_ABHUD_C"));
+
+	if (ABHUDWidgetRef.Class != nullptr)
+	{
+		ABHUDwidgetClass = ABHUDWidgetRef.Class;
+	}
+}
 
 void AABPlayerController::BeginPlay()
 {
@@ -10,4 +22,14 @@ void AABPlayerController::BeginPlay()
 	// 시작 때 마우스가 뷰포트로 입력되어 바로 실행되도록 설정
 	FInputModeGameOnly GameInput;
 	SetInputMode(GameInput);
+
+	// 위젯 생성
+	ABHUDWidget = CreateWidget<UABHUDWidget>(this, ABHUDwidgetClass);
+
+	// 위젯이 문제 없이 생성 됐으면
+	if (ABHUDWidget)
+	{
+		// 위젯을 화면에 추가해 UI가 보일 수 있도록 설정
+		ABHUDWidget->AddToViewport();
+	}
 }
--- Source/ArenaBattleDemo/Player/ABPlayerController.h
+++ Source/ArenaBattleDemo/Player/ABPlayerController.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -15,6 +15,16 @@ class ARENABATTLEDEMO_API AABPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
+	AABPlayerController();
+
 	virtual void BeginPlay() override;
 	
+protected:
+	// 생성할 위젯의 클래스 정보
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = HUD)
+	TSubclassOf<class UABHUDWidget> ABHUDwidgetClass;
+
+	// 생성한 위젯을 객체 정보를 저장할 변수
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = HUD)
+	TObjectPtr<class UABHUDWidget> ABHUDWidget;
 };
